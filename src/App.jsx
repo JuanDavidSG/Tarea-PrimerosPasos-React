cea99d0f23f5fa62bcb86f7a620964e3ded9b0a3
juan david sanchez <Juansangon@unisabana.edu.co>
2023-04-05 15:42:55 -0500

    Crear la funcion para poder añadir el curso

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import React, {useState} from "react";
+
+
+export const App = () => {
+
+    const [curso , setCurso] = useState("");
+    const [cursos, setCursos] = useState([{id: 2, name: 'Ingles 4'}, {id: 3, name: 'Física Mecánica'}])
+
+    const addCurso = (event) => {
+
+        event.preventDefault();
+
+        const newCurso = {
+            id: Math.floor(Math.random() * 100),
+            name: curso
+        }
+
+        const num= newCurso.name
+
+        if (curso.trim().length < 1 || typeof(num) === 'number') return;
+
+        setCursos([...cursos, newCurso])   
+        
+        setCurso('');
+    }      
+  return (
+    <div>App</div>
+  )
+}
